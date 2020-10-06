8862b96b109a0c97b790b94bb5ac56e961d1997f
Gaurav Sunil Bagul <gauravbagul@winjit.com>
2020-10-06 18:50:37 +0530

    changed site key

--- App.js
+++ App.js
@@ -2,21 +2,21 @@ import React from 'react';
 import codePush from 'react-native-code-push';
 import GoogleReCaptcha from './Modal';
 
-const siteKey = '6Ld4GNgUAAAAAALi_QZF2DyemOtBmWYaIn7J8E7l';
+//6Ld1dskZAAAAAGSmnsSpRQ5vX6HRUt0bbmBbxMUT
+// const siteKey = '6Ld4GNgUAAAAAALi_QZF2DyemOtBmWYaIn7J8E7l';
+const siteKey = '6LcgI54UAAAAABGdGmruw6DdOocFpYVdjYBRe4zb';
+
 const baseUrl = 'https://www.google.com/recaptcha/api.js';
 class App extends React.Component {
   onMessage = (event) => {
     console.log('TCL:: App -> onMessage -> event', event);
+    debugger;
     if (event && event.nativeEvent.data) {
       if (['cancel', 'error', 'expired'].includes(event.nativeEvent.data)) {
-        this.captchaForm?.hide();
         return;
       } else {
         console.log('Verified code from Google', event.nativeEvent.data);
-        setTimeout(() => {
-          this.captchaForm?.hide();
-          // do what ever you want here
-        }, 1500);
+        return;
       }
     }
   };
